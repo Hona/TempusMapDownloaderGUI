66d21413aff26269effee2101832c5390394a43c
Luke Parker <lukemparker@outlook.com>
2020-10-01 14:34:52 +1000

    + map data link

--- TempusToMomentumMapRenamer/Utilities/MapDataUtility.cs
+++ TempusToMomentumMapRenamer/Utilities/MapDataUtility.cs
@@ -2,5 +2,10 @@ namespace TempusToMomentumMapRenamer.Utilities
 {
     public static class MapDataUtility
     {
+        /// <summary>
+        ///     Map data from Tempus Hub source.
+        /// </summary>
+        private const string MapDataUri =
+            "https://raw.githubusercontent.com/TheRealHona/TempusHub/master/src/TempusHubBlazor/wwwroot/MapClasses.csv";
     }
 }
