f18476cbf402524efd34a1fd7355b991fd0b4d6e
Luke Parker <lukemparker@outlook.com>
2020-10-01 14:33:26 +1000

    Create mapdata class

---
+++ TempusToMomentumMapRenamer/Models/MapData.cs
@@ -0,0 +1,9 @@
+﻿namespace TempusToMomentumMapRenamer.Models
+{
+    public class MapData
+    {
+        public bool ToCopy { get; set; } = true;
+        public string Name { get; set; }
+        public ClassInfo IntendedClass { get; set; }
+    }
+}
