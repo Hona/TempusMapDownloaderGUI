ec490a7264d23b8b1e41189bb43adda8908a8a20
Luke Parker <lukemparker@outlook.com>
2020-10-01 14:33:09 +1000

    Add default WPF stuff

---
+++ TempusToMomentumMapRenamer/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace TempusToMomentumMapRenamer
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application { }
+}
