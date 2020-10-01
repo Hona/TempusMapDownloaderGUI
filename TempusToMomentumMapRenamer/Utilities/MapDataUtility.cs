657376673f2b765e07ab3f2c7e3c612645c21dd1
Luke Parker <lukemparker@outlook.com>
2020-10-01 14:35:01 +1000

    Add rj/sj prefixes

--- TempusToMomentumMapRenamer/Utilities/MapDataUtility.cs
+++ TempusToMomentumMapRenamer/Utilities/MapDataUtility.cs
@@ -7,5 +7,9 @@ namespace TempusToMomentumMapRenamer.Utilities
         /// </summary>
         private const string MapDataUri =
             "https://raw.githubusercontent.com/TheRealHona/TempusHub/master/src/TempusHubBlazor/wwwroot/MapClasses.csv";
+
+        private const string StickyJumpPrefix = "sj";
+        private const string RocketJumpPrefix = "rj";
+
     }
 }
