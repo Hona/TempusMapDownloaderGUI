cab8064b0d5b7cf680b485ba929111c15b59dce6
Luke Parker <lukemparker@outlook.com>
2020-10-01 14:36:58 +1000

    Add function to copy+rename map (and download if needed)

--- TempusToMomentumMapRenamer/Utilities/MapRenamerUtility.cs
+++ TempusToMomentumMapRenamer/Utilities/MapRenamerUtility.cs
@@ -27,5 +27,51 @@ namespace TempusToMomentumMapRenamer.Utilities
                 yield return Path.Combine(destinationDirectory, mapName + ".bsp");
             }
         }
+
+        private static string RenameMap(MapData mapData, string sourceDirectory, string destinationDirectory, bool downloadMissingMap, Action<string> log)
+        {
+            var mapPath = GetMapSourcePath(sourceDirectory, mapData);
+            var destinationPaths = GetMapDestinationPaths(destinationDirectory, mapData).ToList();
+
+            if (!File.Exists(mapPath))
+            {
+                if (!downloadMissingMap)
+                {
+                    return mapData.Name + " doesn't exist in source";
+                }
+
+                if (destinationPaths.TrueForAll(File.Exists))
+                {
+                    return mapData.Name + " already exists in Momentum Mod files";
+                }
+
+                _webclientSemaphore.Wait();
+
+                // FastDL provides map as bz2, need to decompress before saving to disk
+                var mapUri = "http://tempus.site.nfoservers.com/server/maps/" + mapData.Name + ".bsp.bz2";
+                log($"Downloading and decompressing {mapUri}");
+                var compressedStream = _httpClient.GetStreamAsync(mapUri).GetAwaiter().GetResult();
+                BZip2.Decompress(compressedStream, File.Create(mapPath), true);
+                log($"Decompressed {mapData.Name}");
+
+                _webclientSemaphore.Release();
+
+                // File is decompressed and saved to source dir, safe to continue
+            }
+
+
+            foreach (var destinationPath in destinationPaths)
+            {
+                if (File.Exists(destinationPath))
+                {
+                    log("Map already exists in destination directory: " + destinationPath);
+                    continue;
+                }
+
+                File.Copy(mapPath, destinationPath);
+            }
+
+            return mapData.Name;
+        }
     }
 }
