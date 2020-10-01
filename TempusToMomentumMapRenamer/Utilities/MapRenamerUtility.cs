9f7307fa4df5aea958d560c0058ebfcfff911826
Luke Parker <lukemparker@outlook.com>
2020-10-01 14:35:52 +1000

    Create MapRenamerUtility.cs

---
+++ TempusToMomentumMapRenamer/Utilities/MapRenamerUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.BZip2;
+using TempusToMomentumMapRenamer.Models;
+
+namespace TempusToMomentumMapRenamer.Utilities
+{
+    public static class MapRenamerUtility
+    {
+    }
+}
